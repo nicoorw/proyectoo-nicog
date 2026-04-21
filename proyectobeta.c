3d3e6dafe2d448ffe9a939dec76d3b9b6d8db222
nicoorw <nicolascurriman@gmail.com>
2026-04-21 08:15:49 -0400

    PRUEBA 3

--- proyectobeta.c
+++ proyectobeta.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(){
-    float a, b, c, det, x1, x2, x3, ejey;
+    float a, b, c, dis, x1, x2, x3, ejey;
     printf("Ingresa los valores de la funcion de forma tal que ax² + bx + c\n");
     printf("Ingresa el valor de la variable a: \n");
     scanf("%f", &a);
@@ -14,17 +14,17 @@ int main(){
     scanf("%f", &b);
     printf("Ingresa el valor de la variable c: \n");
     scanf("%f", &c);
-    det = (b*b) - (4*a*c);
-    printf("\n---RESULTADO DEL DISCRIMINANTE---\n");
-    printf("El discriminante es: %2.f \n", det);
-    if ( det > 0 ){
-        x1= (-b + sqrt(det)) /(2.0*a);
-        x2= (-b - sqrt(det)) /(2.0*a);
+    dis = (b*b) - (4*a*c);
+    printf("\n---RESULTADO DEL DISCRIMINANTE(Δ)---\n");
+    printf("El discriminante(Δ) es: %2.f \n", dis);
+    if ( dis > 0 ){
+        x1= (-b + sqrt(dis)) /(2.0*a);
+        x2= (-b - sqrt(dis)) /(2.0*a);
         printf("\n---RESULTADO DEL PUNTO QUE TOCA EN EL EJE X---\n");
         printf("Discriminante > 0 --> 2 soluciones reales \n");
         printf("x1 = %.2f, x2 = %.2f\n", x1,x2);  
     }
-    else if ( det == 0 ){
+    else if ( dis == 0 ){
         x3 = (-b)/(2.0*a);
         printf("Discriminante = 0 --> 1 solucion real \n");
         printf("\n---RESULTADO DEL PUNTO QUE TOCA EN EL EJE X---\n");
@@ -33,27 +33,13 @@ int main(){
     else {
         printf("Discriminante < 0 --> sin soluciones reales\n");
     }
-
-
-    printf("Para saber el corte de la funcion con el eje Y (eje vertical) tenemos que: \n");
-    printf("El corte del eje Y seria (0,c), dejando como valor fijo x = 0, en este caso el corte seria (0,%d) \n", c);
-
     float vrtx, vrty;
-    int det2;
-    det2 = (b*b)-(4*a*c);
+    //int det2;
+    //det2 = (b*b)-(4*a*c);
     vrtx = (-b/(2*a));
-    vrty = -det2/(4*a);
-    
-    printf("El vertice se encuentra en %.1f ,%.1f \n", vrtx,vrty);
-
-
-
-    
-    
-    
-    
-    
-    
+    vrty = -dis/(4*a);
+    printf("\n---RESULTADO DEL VERTICE DE LA FUNCION---\n");
+    printf("El vertice se encuentra en %.2f ,%.2f \n", vrtx,vrty);
     printf("\n---RESULTADO CORTE DE LA FUNCION EN EJE Y---\n");
     printf("(0,%2.f)\n", c);
     printf("¿Te interesa saber como se calcula?(1.Si)(2.No)\n");
@@ -62,6 +48,6 @@ int main(){
         printf("Tenemos que igualar x = 0 en la funcion, esto nos dejara un resultado y este vendria siendo nuestra coordenada Y, la cual resprensentamos de la forma (0,c)\n");
     }  
     else{
-        printf("Jodete");
+        printf("Ok");
     }    return 0;
 }
