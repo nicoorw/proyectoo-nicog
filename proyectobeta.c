77c64ec2194442289f43c20a1a68b35e45862711
nicoorw <nicolascurriman@gmail.com>
2026-04-21 10:19:40 -0400

    LO PUSE MAS LINDO

--- proyectobeta.c
+++ proyectobeta.c
@@ -2,9 +2,10 @@
 #include <math.h>
 
 int main(){
-    float a, b, c, dis, x1, x2, x3, ejey;
+    float a, b, c, dis, x1, x2, x3, vrtx, vrty;
+    int expl;
     printf("Ingresa los valores de la funcion de forma tal que ax² + bx + c\n");
-    printf("Ingresa el valor de la variable a: \n");
+    printf("Ingresa el valor de la variable a: \n"); //INGRESO DE VARIABLES
     scanf("%f", &a);
     if ( a == 0 ){
         printf("La funcion no es cuadratica\n");
@@ -14,40 +15,43 @@ int main(){
     scanf("%f", &b);
     printf("Ingresa el valor de la variable c: \n");
     scanf("%f", &c);
-    dis = (b*b) - (4*a*c);
+    //if (b)
+        //printf("La funcion seria a trabajar seria %fx² "); // TERMINAR ACA CONDICIONALES(GORDO)
+    //--------------------------------------------------------------------------------------------------------
+    dis = (b*b) - (4*a*c); //CALCULO DEL DISCRIMINANTE
     printf("\n---RESULTADO DEL DISCRIMINANTE(Δ)---\n");
-    printf("El discriminante(Δ) es: %2.f \n", dis);
-    if ( dis > 0 ){
-        x1= (-b + sqrt(dis)) /(2.0*a);
-        x2= (-b - sqrt(dis)) /(2.0*a);
+    printf("El discriminante(Δ) es: %2.f \n", dis);//---------------------------------------------------------------------------
+    if ( dis > 0 ){ // QUE NOS DICE EL DISCRIMINANTE
+        x1= (-b + sqrt(dis)) /(2.0*a); //CALCULO SOLUCION 1 SI EL DISCRIMINANTE > 0
+        x2= (-b - sqrt(dis)) /(2.0*a); //CALCULO SOLUCION 2 SI EL DISCRIMINANTE > 0
         printf("\n---RESULTADO DEL PUNTO QUE TOCA EN EL EJE X---\n");
-        printf("Discriminante > 0 --> 2 soluciones reales \n");
+        printf("Discriminante(Δ)  >  0 --> 2 soluciones reales \n");
         printf("x1 = %.2f, x2 = %.2f\n", x1,x2);  
     }
     else if ( dis == 0 ){
-        x3 = (-b)/(2.0*a);
-        printf("Discriminante = 0 --> 1 solucion real \n");
+        x3 = (-b)/(2.0*a); //CALCULO SOLUCION  SI EL DISCRIMINANTE = 0
+        printf("Discriminante(Δ)  =  0 --> 1 solucion real \n");
         printf("\n---RESULTADO DEL PUNTO QUE TOCA EN EL EJE X---\n");
         printf("x1= %.2f\n", x3);
     }
-    else {
-        printf("Discriminante < 0 --> sin soluciones reales\n");
-    }
-    float vrtx, vrty;
+    else { //CASO POR SI EL DISCRIMINANTE < 0 
+        printf("Discriminante(Δ)  <  0 --> sin soluciones reales\n");
+    } //---------------------------------------------------------------------------------------------------------------------------
     //int det2;
     //det2 = (b*b)-(4*a*c);
-    vrtx = (-b/(2*a));
+    vrtx = (-b/(2*a)); //CALCULO DEL VERTICE DE LA FUNCION
     vrty = -dis/(4*a);
     printf("\n---RESULTADO DEL VERTICE DE LA FUNCION---\n");
-    printf("El vertice se encuentra en %.2f ,%.2f \n", vrtx,vrty);
-    printf("\n---RESULTADO CORTE DE LA FUNCION EN EJE Y---\n");
-    printf("(0,%2.f)\n", c);
+    printf("El vertice se encuentra en %.2f ,%.2f \n", vrtx,vrty); //---------------------------------------------------------------
+    printf("\n---CORTE DE LA FUNCION EN EJE Y---\n"); //CORTE DE FUNCION EJE Y
+    printf("El corte del eje Y es: (0,%2.f)\n", c);
     printf("¿Te interesa saber como se calcula?(1.Si)(2.No)\n");
-    scanf("%f", &ejey);
-    if ( ejey == 1 ){
-        printf("Tenemos que igualar x = 0 en la funcion, esto nos dejara un resultado y este vendria siendo nuestra coordenada Y, la cual resprensentamos de la forma (0,c)\n");
+    scanf("%d", &expl);
+    if ( expl == 1 ){
+        printf("Tenemos que igualar x = 0 en la funcion, esto nos dejara un resultado y este vendria siendo nuestra coordenada Y, por lo tanto se representa como (0,c)\n");
     }  
     else{
         printf("Ok");
-    }    return 0;
+    } //-----------------------------------------------------------------------------------------------------------------------------    
+    // return 0;
 }
