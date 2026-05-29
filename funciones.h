1b24bd849bafbecc6d5acf14afd0536b98d83372
frxnciscxvalentina-tech <frxnciscxvalentina@gmail.com>
2026-05-28 21:33:57 -0400

    Update funciones.h

---
+++ funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+ 
+void ExplicarFuncionCuadratica();
+void PedirCoeficientes(float &a, float &b, float &c);
+float CalculoDiscriminante(float a, float b, float c);
+void MostrarTabla(float a, float b, float c);
+void GraficarConsola(float a, float b, float c);
+void CalculoVertice(float a, float b, float dis);
+void CorteEjeY(float c);
+ 
+#endif
