d06191e64fd7208c20b5e1587e63a35903f58999
frxnciscxvalentina-tech <frxnciscxvalentina@gmail.com>
2026-05-28 21:33:35 -0400

    Implement quadratic function menu in main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "funciones.h"
+ 
+int main(){
+    float a, b, c;
+    int opcion;
+ 
+    ExplicarFuncionCuadratica();
+    PedirCoeficientes(a, b, c);
+ 
+    float dis = (b*b)-(4.0*a*c);
+ 
+    do {
+        std::cout << "\n---MENU---\n";
+        std::cout << "1. Calcular discriminante y raices\n";
+        std::cout << "2. Calcular vertice\n";
+        std::cout << "3. Ver corte en eje Y\n";
+        std::cout << "4. Ver tabla de valores\n";
+        std::cout << "5. Ver grafica en consola\n";
+        std::cout << "6. Salir\n";
+        std::cout << "Selecciona una opcion: ";
+        std::cin >> opcion;
+ 
+        if (opcion == 1) {
+            dis = CalculoDiscriminante(a, b, c);
+        } else if (opcion == 2) {
+            CalculoVertice(a, b, dis);
+        } else if (opcion == 3) {
+            CorteEjeY(c);
+        } else if (opcion == 4) {
+            MostrarTabla(a, b, c);
+        } else if (opcion == 5) {
+            GraficarConsola(a, b, c);
+        } else if (opcion == 6) {
+            std::cout << "Hasta luego!\n";
+        } else {
+            std::cout << "Opcion no valida\n";
+        }
+    } while (opcion != 6);
+ 
+    return 0;
+}
