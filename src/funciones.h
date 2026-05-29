a7878eb77abb1ef4fa0af283f12526770a21d2a7
Fernanda Urra Troncoso <61604960+Ferr-U@users.noreply.github.com>
2026-05-29 16:53:58 -0400

    Refactor funciones.h to use classes for calculations

--- src/funciones.h
+++ src/funciones.h
@@ -1,25 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-// Explica al usuario en que consiste una funcion cuadratica
-void ExplicarFuncionCuadratica();
+class Funcion {
+    private:
+        float a, b, c;
+        
+    public:
+        Funcion(float a, float b, float c);
+        float A();
+        float B();
+        float C();
 
-// Pide al usuario los coeficientes a, b, c y valida que a sea distinto de 0
+};
+
+class Calculadora {
+    private:
+        Funcion parabola;
+    public:
+    Calculadora(float a,float b,float c);
+
+    float CalculoDiscriminante();
+    void CalculoVertice(float dis);
+    void CorteEjeY();
+    void MostrarTabla();
+    void GraficarConsola();
+};
+
 void PedirCoeficientes(float &a, float &b, float &c);
-
-// Calcula el discriminante y las raices de la funcion, retorna el discriminante
-float CalculoDiscriminante(float a, float b, float c);
-
-// Calcula y muestra el vertice de la parabola
-void CalculoVertice(float a, float b, float dis);
-
-// Muestra el corte de la funcion con el eje Y
-void CorteEjeY(float c);
-
-// Muestra una tabla de valores de f(x) para x entre -5 y 5
-void MostrarTabla(float a, float b, float c);
-
-// Genera una grafica aproximada de la funcion en consola usando asteriscos
-void GraficarConsola(float a, float b, float c);
+void ExplicarFuncionCuadratica();
 
 #endif
