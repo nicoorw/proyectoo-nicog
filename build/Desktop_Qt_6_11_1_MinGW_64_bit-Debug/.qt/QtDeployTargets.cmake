c69b714467d80fb56c38cfe597d781a2736128db
nicoorw <nicolascurriman@gmail.com>
2026-06-23 19:39:05 -0400

    Arreglar gitignore y eliminar carpetas de build

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CalculadoraCuadratica_FILE C:/Users/PC/Desktop/SEMINARIO DE PROGRA/PROYECTO/proyectoo-nicog/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CalculadoraCuadratica.exe)
-set(__QT_DEPLOY_TARGET_CalculadoraCuadratica_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CalculadoraCuadratica_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
